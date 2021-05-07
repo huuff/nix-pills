0f16c595260a8defcc4c74901990aaff8ff785a6
Haf <haf@protonmail.ch>
2021-05-07 11:13:41 +0200

    finished pill 14

--- TODO.md
+++ TODO.md
@@ -1,2 +1,1 @@
-* Move tars to a specific directory `tars` to prevent chasing them across pills
 * In pill 12 `with-png` and inputs' graphviz don't really work with png, check [this issue](https://github.com/NixOS/nix-pills/issues/107)
--- pill-14/default.nix
+++ pill-14/default.nix
@@ -5,10 +5,10 @@ let
   callPackage = path: overrides:
     let f = import path;
     in lib.makeOverridable f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
-  pkgs = with nixpkgs; {
+  pkgs = with nixpkgs; rec {
     mkDerivation = import ../pill-12/inputs/autotools.nix nixpkgs;
     hello = callPackage ../pill-12/inputs/hello.nix { };
     graphviz = callPackage ../pill-12/inputs/graphviz.nix { };
-    graphvizCore = callPackage ../pill-12/inputs/graphviz.nix { gdSupport = false; };
+    graphvizCore = graphviz.override { gdSupport = false; };
   };
 in pkgs
