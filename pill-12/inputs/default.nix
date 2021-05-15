18516c47ecf96382eb5c8bf0e684b869d5260b80
Haf <haf@protonmail.ch>
2021-05-15 13:27:59 +0200

    fixed the reproducibility issue of graphviz completely

--- pill-12/inputs/default.nix
+++ pill-12/inputs/default.nix
@@ -1,6 +1,10 @@
 let 
-  pkgs = import <nixpkgs> {};
-  mkDerivation = import ./autotools.nix pkgs;
+  pkgs = import (builtins.fetchGit {
+    url = "https://github.com/nixos/nixpkgs";
+    ref = "refs/heads/release-14.12";
+    rev = "770822493e3962d795739fafd522e771bfe06e3d";
+  }) { };
+  mkDerivation = import ../with-png/autotools.nix pkgs;
 in with pkgs; {
   hello = import ./hello.nix { inherit mkDerivation; };
   graphviz = import ./graphviz.nix { inherit mkDerivation gd fontconfig libjpeg bzip2; };
