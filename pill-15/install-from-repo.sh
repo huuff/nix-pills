a0862495ad46afcba9f773ba320c126c7b21dda1
Haf <haf@protonmail.ch>
2021-05-15 13:10:07 +0200

    relative path for pill 15

--- pill-15/install-from-repo.sh
+++ pill-15/install-from-repo.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 # not too portable
-export NIX_PATH="mypkgs=/home/haf/nix-pills/pill-14:$NIX_PATH"
+export NIX_PATH="mypkgs=../pill-14:$NIX_PATH"
 nix-env -f '<mypkgs>' -iA graphviz
