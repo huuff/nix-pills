b1a7e94f2775fc48d07c40587f75181948902469
Haf <haf@protonmail.ch>
2021-05-16 13:31:42 +0200

    pill 19 flake

---
+++ pill-19/flake/flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Hello";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;
+
+  outputs = { self, nixpkgs }: {
+
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "hello";
+        src = ../../tars/hello-2.10.tar.gz;
+      };
+  };
+}
